27fb73ce76e093a62b9d5ae64561f92513173a18
Jonathan Giller <63469038+Jgiller-prog@users.noreply.github.com>
2020-04-10 16:09:58 -0700

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -1,1 +1,11 @@
-
+public class Supplier{
+  private int numGoggles;
+  private int numFaceMasks;
+  private int numFaceShields;
+  private int numN95Masks;
+  private int numGloves;
+  private int numSewnMaskCovers;
+  
+  public Supplier(){
+  }
+}
